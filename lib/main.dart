816147065a4af4d1b6617bf2767122cae1870e1f
afifahnisa17 <afifahnisa041@gmail.com>
2025-09-24 17:29:21 +0700

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -26,16 +26,32 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+  
 }
+
 
 class MyHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+  Widget build(BuildContext context) {           // ← 1
+    var appState = context.watch<MyAppState>();  // ← 2
 
-    return Scaffold(
-      body: Column(
-        children: [Text('A random idea:'), Text(appState.current.asLowerCase)],
+    return Scaffold(                             // ← 3
+      body: Column(                              // ← 4
+        children: [
+          Text('A random AWESOME idea:'),        // ← 5
+          Text(appState.current.asLowerCase),    // ← 6
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next'),
+          ),
+        ],                                       // ← 7
       ),
     );
   }
